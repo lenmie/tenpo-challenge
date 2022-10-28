cdda8454b61d6c50e51c69636f2df6cbb59a1aba
lenmie <enci.javier@yahoo.com>
2022-10-28 20:23:58 -0300

    added react native maps and google maps android sdk;

--- src/app/screens/AddDelivery/MapContainer.styled.tsx
+++ src/app/screens/AddDelivery/MapContainer.styled.tsx
@@ -1,8 +1,14 @@
 import styled from '@emotion/native';
 import { View } from 'react-native';
 import { colors } from '../../../constants/colors';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export const Container = styled(View)`
   height: 256px;
   background-color: ${colors.ligthGrey};
+`;
+
+export const AddressMap = styled(MapView)`
+  height: 100%,
+  width: 100%,
 `;
--- src/app/screens/AddDelivery/MapContainer.tsx
+++ src/app/screens/AddDelivery/MapContainer.tsx
@@ -1,6 +1,41 @@
 import React from 'react';
-import { Container } from './MapContainer.styled';
+import { AddressMap, Container } from './MapContainer.styled';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+import { StyleSheet } from 'react-native';
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
 export default function MapContainer() {
-  return <Container />;
+  return (
+    <Container>
+      {/* <AddressMap
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      /> */}
+      <MapView
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}></MapView>
+    </Container>
+  );
 }
