084678a384cfddbb890fcc9e7d4c64b37271fa44
lenmie <enci.javier@yahoo.com>
2022-11-01 13:54:28 -0300

    added fonts, colors and rebass theme

---
+++ src/constants/theme.ts
@@ -0,0 +1,20 @@
+export const theme = {
+  breakpoints: [32, 48, 64, 80],
+  space: [0, 4, 8, 16, 32, 64, 128],
+  fonts: {
+    roboto: 'Roboto',
+    gotham: 'Gotham',
+  },
+  fontSizes: [12, 14, 16, 18, 24, 32, 42, 64, 72, 96],
+  fontWeights: {
+    normal: 400,
+    bold: 700,
+  },
+  colors: {
+    black: '#000',
+    white: '#fff',
+    grey: ['#00000029', '#ADADAD'],
+    green: ['#D4F9F5', '#008F7E', '#00BAA4', '#3FAEA1', '#75E7D974'],
+  },
+  radii: [0, 2, 4],
+};
